f88aa788b08a04631c4539132a31c57cd47db32c
Léandro DA SILVA <leothefrench@gmail.com>
2026-02-10 15:49:43 +0100

    fix: prevent stripe from crashing the build process

--- apps/web/src/lib/stripe.ts
+++ apps/web/src/lib/stripe.ts
@@ -1,6 +1,8 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const apiKey = process.env.STRIPE_SECRET_KEY || '';
+
+export const stripe = new Stripe(apiKey, {
   apiVersion: '2026-01-28.clover',
   typescript: true,
 });
